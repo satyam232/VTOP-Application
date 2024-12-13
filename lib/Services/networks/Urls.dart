ffbf4485f8ae693d408b71526f6f91c34cb0706e
Satyam Rana <60211993+satyam232@users.noreply.github.com>
2024-12-13 07:41:45 +0530

    Update Urls.dart

--- lib/Services/networks/Urls.dart
+++ lib/Services/networks/Urls.dart
@@ -1,7 +1,6 @@
 class AllUrls {
-  static String base = 'http://35.154.8.192:8052';
-  // static String base = 'https://vtop-api-1.onrender.com';
-  // static String base = 'http://192.168.219.65:8502';
+  
+  static String base = 'http://192.168.219.65:8502';
   static String login = '$base/vtop/login';
   static String timetable = '$base/vtop/timetable';
   static String attendenceUrl = '$base/vtop/attendence';
